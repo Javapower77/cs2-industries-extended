dd012a8c3859458fb59bd7fc5991a7c74d250cc8
Javapower77 <javapower1977@gmail.com>
2024-12-16 11:03:22 -0300

    clean up code

--- UI/src/mods/TestFields/TestFieldsStorage.tsx
+++ UI/src/mods/TestFields/TestFieldsStorage.tsx
@@ -14,52 +14,17 @@ interface StorageSectionComponent {
     resources: Array<object>;
 }
 
-const InfoSectionTheme: Theme | any = getModule(
-	"game-ui/game/components/selected-info-panel/shared-components/info-section/info-section.module.scss",
-	"classes"
-);
-
-const InfoRowTheme: Theme | any = getModule(
-	"game-ui/game/components/selected-info-panel/shared-components/info-row/info-row.module.scss",
-	"classes"
-)
-
-const InfoSection: any = getModule( 
-    "game-ui/game/components/selected-info-panel/shared-components/info-section/info-section.tsx",
-    "InfoSection"
-)
-
-const InfoRow: any = getModule(
-    "game-ui/game/components/selected-info-panel/shared-components/info-row/info-row.tsx",
-    "InfoRow"
-)
-
 const StorageSection: any = getModule(
     "game-ui/game/components/selected-info-panel/selected-info-sections/building-sections/storage-section.tsx",
     "StorageSection"
 )
 
 
-function handleClick(eventName : string) {
-    // This triggers an event on C# side and C# designates the method to implement.
-    trigger(mod.id, eventName);
-}
-
-const descriptionToolTipStyle = getModule("game-ui/common/tooltip/description-tooltip/description-tooltip.module.scss", "classes");
-
 interface GoodResources {
     key: any;
     amount: any;
 }
 
-function DescriptionTooltip(tooltipTitle: string | null, tooltipDescription: string | null) : JSX.Element {
-    return (
-        <>
-            <div className={descriptionToolTipStyle.title}>{tooltipTitle}</div>
-            <div className={descriptionToolTipStyle.content}>{tooltipDescription}</div>
-        </>
-    );
-}
 
 const IsBoostedFactory$ = bindValue<string>(mod.id, "IsBoostedFactory");
 const GoodProduction$ = bindValue<string>(mod.id, "GoodProduction");
